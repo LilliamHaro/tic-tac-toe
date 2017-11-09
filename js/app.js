5fbd42c376e086352104effba6ff4ef098713fe1
LilliamHaro <qualia950@gmail.com>
2017-11-09 02:32:40 -0500

    subiendo avance del ejercicio tic-tac-toe

---
+++ js/app.js
@@ -0,0 +1,26 @@
+
+window.onload = function() {
+    var board = document.querySelector('.board-js');
+
+board.addEventListener('click', addX);
+};
+var centinel = true;
+function addX(event){
+    if(event.target.matches('td') && event.target.textContent === ''){
+        if(centinel) {
+            event.target.textContent= 'X';
+        centinel = false;
+        } else {
+            event.target.textContent='0';
+        centinel = !centinel;
+        }
+    }
+}
+
+
+//agregando reset
+
+function reset() {
+  window.location.reload(); // recargar la pagina -location se refiere a la url
+}
+document.getElementById('restart').addEventListener('click', reset); //addEventListener(evento,funcion a ejecutar)
